971d614c0965a860e0dbf81eda448ef232cb36c4
Jilin Hu <hujilin1229@163.com>
2019-09-06 16:07:23 +0400

    add support to traffic4cast

--- data_loader/data_utils.py
+++ data_loader/data_utils.py
@@ -9,7 +9,8 @@ from utils.math_utils import z_score
 
 import numpy as np
 import pandas as pd
-
+import os
+import h5py
 
 class Dataset(object):
     def __init__(self, data, stats):
@@ -53,7 +54,6 @@ def seq_gen(len_seq, data_seq, offset, n_frame, n_route, day_slot, C_0=1):
             tmp_seq[i * n_slot + j, :, :, :] = np.reshape(data_seq[sta:end, :], [n_frame, n_route, C_0])
     return tmp_seq
 
-
 def data_gen(file_path, data_config, n_route, n_frame=21, day_slot=288):
     '''
     Source file load and dataset generation.
@@ -88,6 +88,110 @@ def data_gen(file_path, data_config, n_route, n_frame=21, day_slot=288):
     dataset = Dataset(x_data, x_stats)
     return dataset
 
+def seq_gen_train_traffic4cast(data_seq, offset, n_frame, node_pos, C_0=3):
+    '''
+    Generate data in the form of standard sequence unit.
+    :param data_seq: np.ndarray, source data / time-series.
+    :param offset:  int, the starting index of different dataset type.
+    :param n_frame: int, the number of frame within a standard sequence unit,
+                         which contains n_his = 12 and n_pred = 9 (3 /15 min, 6 /30 min & 9 /45 min).
+    :param num_nodes: int, the number of nodes in the graph.
+    :param C_0: int, the size of input channel.
+    :return: np.ndarray, [len_seq, n_frame, n_route, C_0].
+    '''
+
+    num_data = data_seq.shape[0]
+    n_slot = num_data - n_frame + 1
+    tmp_seqs = []
+
+    for i in range(0, n_slot, offset):
+        tmp_seq = data_seq[i:i+n_frame, node_pos[:, 0], node_pos[:, 1], :C_0]
+        tmp_seqs.append(tmp_seq)
+
+    return np.stack(tmp_seqs, axis=0)
+
+
+def data_gen_traffic4cast(file_path, process_dir, node_pos, seq_len, horizon, data_start,val_indices,
+                          train_ratios=0.8, val_ratios=0.1):
+    '''
+    Source file load and dataset generation.
+    :param file_path: str, the file path of data source.
+    :param data_config: tuple, the configs of dataset in train, validation, test.
+    :param n_route: int, the number of routes in the graph.
+    :param n_frame: int, the number of frame within a standard sequence unit,
+                         which contains n_his = 12 and n_pred = 9 (3 /15 min, 6 /30 min & 9 /45 min).
+    :param day_slot: int, the number of time slots per day, controlled by the time window (5 min as default).
+    :return: dict, dataset that contains training, validation and test with stats.
+    '''
+
+    train_data_nz_file = process_dir + 'stgcn_seq{}_horizon{}_train_data.npz'.format(seq_len, horizon)
+    val_data_nz_file = process_dir + 'stgcn_seq{}_horizon{}_train_data.npz'.format(seq_len, horizon)
+    test_data_nz_file = process_dir + 'stgcn_seq{}_horizon{}_train_data.npz'.format(seq_len, horizon)
+
+    if os.path.exists(train_data_nz_file):
+        seq_train = np.load(train_data_nz_file)
+        seq_train = seq_train['seq_data']
+
+        seq_val = np.load(val_data_nz_file)
+        seq_val = seq_val['seq_data']
+
+        seq_test = np.load(test_data_nz_file)
+        seq_test = seq_test['seq_data']
+    else:
+        files = os.listdir(file_path)
+        data_list = []
+        num_files = len(files)
+        n_train = int(num_files * train_ratios)
+        for f in files[:n_train]:
+            try:
+                data_file = h5py.File(os.path.join(file_path, f), 'r')
+                raw_data = data_file['array']
+                data_file.close()
+                raw_data = raw_data[data_start:]
+                tmp_data = seq_gen_train_traffic4cast(raw_data, horizon, seq_len+horizon, node_pos, C_0=3)
+                data_list.append(tmp_data)
+            except:
+                print(file_path + '/' + f)
+        seq_train = np.concatenate(data_list, axis=0)
+        np.savez_compressed(train_data_nz_file, seq_data=seq_train)
+
+        n_val = int(num_files * val_ratios)
+        seq_val = []
+        for f in files[n_train:n_train+n_val]:
+            try:
+                data_file = h5py.File(os.path.join(file_path, f), 'r')
+                raw_data = data_file['array']
+                data_file.close()
+                seq_val += [raw_data[i-seq_len:i+horizon, node_pos[:, 0], node_pos[:, 1], :] for i in val_indices]
+            except:
+                print(file_path + '/' + f)
+        seq_val = np.concatenate(seq_val, axis=0)
+        np.savez_compressed(val_data_nz_file, seq_data=seq_val)
+
+        seq_test = []
+        for f in files[n_train + n_val:]:
+            try:
+                data_file = h5py.File(os.path.join(file_path, f), 'r')
+                raw_data = data_file['array']
+                data_file.close()
+                seq_test += [raw_data[i - seq_len:i + horizon, node_pos[:, 0], node_pos[:, 1], :] for i in val_indices]
+            except:
+                print(file_path + '/' + f)
+        seq_test = np.concatenate(seq_test, axis=0)
+        np.savez_compressed(test_data_nz_file, seq_data=seq_test)
+
+    # x_stats: dict, the stats for the train dataset, including the value of mean and standard deviation.
+    x_stats = {'mean': np.mean(seq_train), 'std': np.std(seq_train)}
+
+    # x_train, x_val, x_test: np.array, [sample_size, n_frame, n_route, channel_size].
+    x_train = z_score(seq_train, x_stats['mean'], x_stats['std'])
+    x_val = z_score(seq_val, x_stats['mean'], x_stats['std'])
+    x_test = z_score(seq_test, x_stats['mean'], x_stats['std'])
+
+    x_data = {'train': x_train, 'val': x_val, 'test': x_test}
+    dataset = Dataset(x_data, x_stats)
+
+    return dataset
 
 def gen_batch(inputs, batch_size, dynamic_batch=False, shuffle=False):
     '''
---
+++ main_traffic4cast.py
@@ -0,0 +1,87 @@
+# @Time     : Jan. 02, 2019 22:17
+# @Author   : Veritas YIN
+# @FileName : main.py
+# @Version  : 1.0
+# @Project  : Orion
+# @IDE      : PyCharm
+# @Github   : https://github.com/VeritasYin/Project_Orion
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from os.path import join as pjoin
+
+import tensorflow as tf
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+tf.Session(config=config)
+
+from utils.math_graph import *
+from data_loader.data_utils import *
+from models.trainer import model_train
+from models.tester import model_test
+import scipy.sparse as sp
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_route', type=int, default=228, help='Number of Nodes')
+parser.add_argument('--horizon', type=int, default=6)
+parser.add_argument('--seq_len', type=int, default=3)
+parser.add_argument('--batch_size', type=int, default=50)
+parser.add_argument('--epoch', type=int, default=50)
+parser.add_argument('--save', type=int, default=10)
+parser.add_argument('--ks', type=int, default=3)
+parser.add_argument('--kt', type=int, default=3)
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--opt', type=str, default='RMSProp')
+parser.add_argument('--city', type=str, default='Berlin', help='"Berlin", "Istanbul", "Moscow"')
+parser.add_argument('--data_dir', type=str, default='../', help='directory to load the data')
+parser.add_argument('--least_ratio', type=float, default=0.5, help='ratio to select pixels')
+parser.add_argument('--inf_mode', type=str, default='merge')
+args = parser.parse_args()
+print(f'Training configs: {args}')
+
+utcPlus2 = [30, 69, 126, 186, 234]
+utcPlus3 = [57, 114, 174,222, 258]
+
+Ks, Kt = args.ks, args.kt
+# blocks: settings of channel size in st_conv_blocks / bottleneck design
+blocks = [[1, 32, 64], [64, 32, 128]]
+
+data_path = pjoin(args.data_dir, args.city)
+process_data_dir = pjoin(data_path, 'process_{}'.format(args.least_ratio))
+if not os.path.isdir(process_data_dir):
+    os.mkdir(process_data_dir)
+adj_file = pjoin(process_data_dir, 'adj_{}.npz'.format(args.least_ratio))
+node_pos_file = pjoin(process_data_dir, 'node_pos_{}.npy'.format(args.least_ratio))
+
+W = sp.load_npz(adj_file).toarray()
+node_pos = np.load(node_pos_file)
+# num of nodes
+n = node_pos.shape[0]
+
+# Calculate graph kernel
+L = scaled_laplacian(W)
+# Alternative approximation method: 1st approx - first_approx(W, n).
+Lk = cheb_poly_approx(L, Ks, n)
+tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
+
+raw_data_path = pjoin(data_path, 'train_val') #the folder contain train and validation as the data used in the paper
+
+indicies = utcPlus3
+if args.city == 'Berlin':
+    indicies = utcPlus2
+
+traffic4cast_data = data_gen_traffic4cast(raw_data_path, process_data_dir, node_pos, args.seq_len, args.horizon,
+                                          data_start=0, val_indices=indicies, train_ratios=0.8, val_ratios=0.1)
+# Data Preprocessing
+# data_file = f'PeMSD7_V_{n}.csv'
+# n_train, n_val, n_test = 34, 5, 5
+# PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
+print(f'>> Loading dataset with Mean: {traffic4cast_data.mean:.2f}, STD: {traffic4cast_data.std:.2f}')
+
+if __name__ == '__main__':
+    model_train(traffic4cast_data, blocks, args)
+    model_test(traffic4cast_data, traffic4cast_data.get_len('test'), args.seq_len, args.horizon, args.inf_mode)
