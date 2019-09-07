81bc5e2b2f55c29044e6464b0ed4142051de3f82
Jilin_Mac <hujilin1229@163.com>
2019-09-07 20:58:28 +0400

    add sp tensor

--- models/layers.py
+++ models/layers.py
@@ -83,13 +83,13 @@ def temporal_conv_layer(x, Kt, c_in, c_out, act_func='relu'):
         wt = tf.get_variable(name='wt', shape=[Kt, 1, c_in, 2 * c_out], dtype=tf.float32)
         tf.add_to_collection(name='weight_decay', value=tf.nn.l2_loss(wt))
         bt = tf.get_variable(name='bt', initializer=tf.zeros([2 * c_out]), dtype=tf.float32)
-        x_conv = tf.nn.conv2d(x, wt, strides=[1, 1, 1, 1], padding='VALID') + bt
+        x_conv = tf.nn.conv2d(x, wt, strides=[1, 1, 1, 1], padding='SAME') + bt
         return (x_conv[:, :, :, 0:c_out] + x_input) * tf.nn.sigmoid(x_conv[:, :, :, -c_out:])
     else:
         wt = tf.get_variable(name='wt', shape=[Kt, 1, c_in, c_out], dtype=tf.float32)
         tf.add_to_collection(name='weight_decay', value=tf.nn.l2_loss(wt))
         bt = tf.get_variable(name='bt', initializer=tf.zeros([c_out]), dtype=tf.float32)
-        x_conv = tf.nn.conv2d(x, wt, strides=[1, 1, 1, 1], padding='VALID') + bt
+        x_conv = tf.nn.conv2d(x, wt, strides=[1, 1, 1, 1], padding='SAME') + bt
         if act_func == 'linear':
             return x_conv
         elif act_func == 'sigmoid':
